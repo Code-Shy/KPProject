27ba1ba0f5af5f17aebc731d5cebd86a393cd73f
Simple <2369013090@qq.com>
2022-03-17 00:29:03 +0800

    New project

---
+++ src/com/nwnu/kp/Main.java
@@ -0,0 +1,13 @@
+package com.nwnu.kp;
+
+/**
+ * @Package com.nwnu.kp
+ * @Description
+ * @Author Simple
+ * @Date 2022/3/17
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
