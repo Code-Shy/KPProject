20e12f0f83e70e62632c5b75380efda2ec76dd2a
Simple <2369013090@qq.com>
2022-03-18 00:08:22 +0800

    PicUtils.scatterPlotPaint 可以将散点图导出

--- src/pers/gqh/kp/utils/DataUtils.java
+++ src/pers/gqh/kp/utils/DataUtils.java
@@ -1,6 +1,7 @@
 package pers.gqh.kp.utils;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,29 +11,38 @@ import java.util.List;
  * @Date 2022/3/17
  **/
 public class DataUtils {
+    public static void main(String[] args) throws IOException {
+        List weight = new ArrayList();
+        List value = new ArrayList();
+        loadData("D:\\KP\\Data\\beibao0.in", weight, value);
+    }
+
+
 
     /**
      *
      * @param fileName
      * @throws IOException
      */
-    public static void loadData (String fileName, List w, List v) throws IOException {
+    public static void loadData (String fileName, List weight, List value) throws IOException {
         try (FileReader reader = new FileReader(fileName);
              BufferedReader br = new BufferedReader(reader)) {
             String line;
             while ((line = br.readLine()) != null) {
                 // 一次读入一行数据
                 String[] s =line.split(" ");
-                w.add(s[0]);
-                v.add(s[1]);
+                weight.add(Integer.parseInt(s[0]));
+                value.add(Integer.parseInt(s[1]));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(w);
-        System.out.println(v);
+        System.out.println(weight);
+        System.out.println(value);
     }
 
+
     public static void sortByVW() {
+
     }
 }
--- src/pers/gqh/kp/utils/PicUtils.java
+++ src/pers/gqh/kp/utils/PicUtils.java
@@ -1,19 +1,17 @@
 package pers.gqh.kp.utils;
 
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
+import org.jfree.chart.*;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.*;
-import org.jfree.ui.RefineryUtilities;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @Package pers.gqh.kp.utils
@@ -23,29 +21,48 @@ import java.awt.*;
  **/
 public class PicUtils {
 
-    public static void main(String[] args)   {
+    public static void main(String[] args) throws IOException {
 
         scatterPlotPaint();
     }
 
-    public static void scatterPlotPaint() {
+    public static void scatterPlotPaint() throws IOException {
         double[][] a = {{1, 2, 3, 5, 6, 7, 8},
-                        {4, 5, 6, 7, 8, 9, 10}};
+                {4, 5, 6, 7, 8, 9, 10}};
 
         DefaultXYDataset defaultXYDataset = new DefaultXYDataset();
-        defaultXYDataset.addSeries("Data", a);
+        defaultXYDataset.addSeries(" ", a);
         JFreeChart chart = ChartFactory.createScatterPlot("", "Weight", "Value", defaultXYDataset,
-                PlotOrientation.VERTICAL, true, true, true);//设置表头，x轴，y轴，name表示问题的类型
-        ChartFrame frame = new ChartFrame("KP数据散点图", chart, true);
+                PlotOrientation.VERTICAL, true, true, false);//设置表头，x轴，y轴，name表示问题的类型
         XYPlot xyplot = (XYPlot) chart.getPlot();
         xyplot.setBackgroundPaint(Color.white);//设置背景面板颜色
         ValueAxis valueAxis = xyplot.getDomainAxis();
         valueAxis.setAxisLineStroke(new BasicStroke(1.0f));//设置坐标轴粗细
+
+        //输出PNG文件
+        OutputStream os_png = new FileOutputStream("D:/1.png");
+        ChartUtilities.writeChartAsPNG(os_png, chart, 500, 500);
+
+        //以面板显示
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 400));
+
+        //创建一个主窗口来显示面板
+        JFrame frame = new JFrame("散点图");
+        frame.setLocation(500, 400);
+        frame.setSize(600, 500);
+
+        //将主窗口的内容面板设置为图表面板
+        frame.setContentPane(chartPanel);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
 
 
+
+
+
     }
+
 
 }
